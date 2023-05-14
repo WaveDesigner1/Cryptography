41259e44a412b0672290da5315c1fd2bcb587286
WaveDesigner1 <118994140+WaveDesigner1@users.noreply.github.com>
2023-05-14 13:06:46 +0200

    Add files via upload
    Plik o nazwie Klucz_jednorazowy zawiera kod źródłowy do skryptu szyfrującego kluczem jednorazowym. 
    
    Algorytm spełnia zasadę Shannona traktujący o jednoznaczości klucza.
    	
    Funkcja keyGen generuje klucz z przestrzeni alfabetu od A do Z na podstawie ilości znaków w tekscie podanym przez użytkownika. 
    
    Następne bloki konwertują tekst oraz wygenerowany klucz do wartości bitowych i mieszają je ze sobą przy pomocy alternatywy rozłącznej.
    
    Po podaniu właściwego klucza skrypt wyświetla tekst jawny. 
    
    W realnych warunkach klucz nie powinien być przechowywany w pamięci podręcznej programu i powinien być chroniony przez użySZKODnika. 
    Skrypt napisałem jako podsmumowanie działu i z uwagi na jego edukacyjną fukncję, klucz wyświetlany jest terminalu. 
    
    	"Nie można wymagać aby metoda szyfrowania była tajna. Przejęcie metody przez wroga nie może powodować problemów.  
    	 Bezpieczeństwo musi polegać wyłącznie na poufności klucza"~ Auguste Kerckoffs
    	 
    Dobra, żeby nie było tak kolorowo. Pomimo zalety jaką jest fakt, że do wiadomości o takiej samej treści generowany jest zawsze inny klucz,
    to jego długość będzie rosła wraz z długością wiadomości. ;) 
    
    Pammiętaj mordko "Wróg zna system!"
    
    Wersja pythona: Python 3.11.2 (Jak Ci coś nie działa to sprawdź czy masz taką samą wersję interpretera na swoim zacnym Windowsie, zainstaluj odpowiednie biblioteki)
    
    Wiedza oparta na : Shannon W.Bray "Algorytmy kryptograficzne w Pythonie - Wprowadzenie"

---
+++ Klucz_jednorazowy.py
@@ -0,0 +1,96 @@
+import secrets
+from binascii import hexlify, unhexlify
+import sys
+from time import sleep
+import os
+a = 1
+
+while a == 1:
+
+	msg = input('Wpisz wiadomość do zaszyfrowania, nie używaj polskich znaków ')
+	key = '' 
+	
+
+	def keyGen(msg, key):
+		for i in range(len(msg)):
+			key += secrets.choice('ABCDEFGHIJKLMNOPQRSTUVWYZ!@#$%^&*()-=+')
+		
+		
+		return key
+
+	key = keyGen(msg, key)	 
+	print('Wiadomość: ', msg)
+	print('Twój klucz: ', key + ' Skopiuj lub zapamiętaj')
+	i = 1	
+	while i < 10:
+				
+		sleep(1)
+		print(i)
+			
+		i += 1
+		if i == 10:
+			os.system('clear')
+			
+	def KeyEnc(msg, key):
+
+		EncMsg = msg.encode()
+		hexmsg = hexlify(EncMsg)
+		hexint = int(hexmsg, 16)
+		
+		keyGen = key.encode()
+		hexkey = hexlify(keyGen)
+		keyint = int(hexkey, 16) ^ hexint
+		return keyint	
+
+	msg = KeyEnc(msg,key)
+	print()
+	print('Szyfrogram: ', msg)
+	print()
+
+	def Dec(msg, key):
+		
+		msgDec = key ^ msg
+		back2hex = format(msgDec , 'x')
+		jawny = unhexlify(back2hex)
+		print(jawny)
+
+	Jawny = input('Podaj klucz aby odczytać tekst jawny: ')
+
+	if Jawny == key:
+		key = key.encode()
+		key = hexlify(key)
+		key = int(key, 16)
+		print(key)
+		msgDec = Dec(msg, key)
+		
+		
+	else:
+		print('Podałeś nieprawidłowy klucz...')
+	
+			
+	exit = 'a'
+	while exit != 0:
+	
+		exit = input('Czy chcesz zaszyfrować kolejną wiadomość? (T/N)')
+			
+		if exit == 'T':
+			a = 1
+			break
+		elif exit == 't':
+			a = 1
+			break
+		elif exit == 'N':
+			a = 0
+			break
+		elif exit == 'n':
+			a = 0
+			break
+		else:
+			continue
+		
+	
+	
+sys.exit(0)
+	
+
+
